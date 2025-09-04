4f51bd39050694d5a69a7a4db346aa044ef4926b
know58286anonymous-stack <know58286anonymous@gmail.com>
2025-09-04 18:00:53 +0545

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    # Render the welcome page with animated "Team SPY" text
+    # Render the welcome page with animated "lets forward" text
     return render_template("welcome.html")
 
 if __name__ == "__main__":
     # Default to port 5000 if PORT is not set in the environment
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
